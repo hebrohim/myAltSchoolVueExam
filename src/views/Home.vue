07a39e1774edcdea71a2b147b6376a6120b1592b
hebrohim <raheemibrohim@mail.com>
2023-03-17 18:15:21 +0100

    feat: finish structure,increment and decrement functions with routing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,23 @@
+
 <script setup>
-import { RouterLink } from 'vue-router'
-import Img from '../assets/calc.png'
+
+import { RouterLink } from "vue-router";
+
 
 </script>
 
 <template>
-    <main class="wrapper  h-auto  main-bg  md:px-5 md:py-3">
-        <div class="w-full  text-white py-6 md:py-4 flex justify-end  items-center px-5  ">
-            <RouterLink to="/counter" class="text-2xl font-bold"> Counter</RouterLink>
-        </div>
+  <section class = "homeSection">
+   
+            <h5 >
+              Welcome to my Vue counter App
+            </h5>
 
-        <div class="w-full mt-10  h-full ">
-
-            <div class=" grid grid-cols-2 text-white w-full h-full ">
-                <div class="col-span-2 md:col-span-1 ">
-                    <div class="w-full h-full centered-col gap-9 px-6">
-                        <h1 class="w-10/12  text-5xl font-bold text-start ">Thinking of using a good and proven <span
-                                class="">counter </span> ? i've got you covered. </h1>
-
-                        <p class="w-10/12 text-xl ">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed nulla, veritatis adipisci mollitia
-                            rem tenetur!
-                        </p>
-
-                        <ul class="md:mt-5  mt-3 p-4 border border-slate-200  italic    shadow-lg  ">
-                            <li class="- text-xl">Add and Subtract functionality</li>
-                            <li class="- text-xl">Use of Composables</li>
-                            <li class="- text-xl">Set counter</li>
-                            <li class="- text-xl">Reset</li>
-                            <li class="- text-xl">Vuex</li>
-                        </ul>
-
-                        <RouterLink to="/counter" class="hidden md:block">
-                            <button class="btn"> Explore</button>
-                        </RouterLink>
-                    </div>
-                </div>
-                <div class="col-span-2 md:col-span-1  ">
-                    <div class="flex justify-center items-center h-full">
-                        <img :src="Img" alt="photo" class="" />
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <div class="mt-10"></div>
-        <RouterLink to="/counter" class="md:hidden px-6 mt-10 ">
-            <button class="btn"> Explore</button>
-        </RouterLink>
-        <div class="h-40 ">
-
-        </div>
-    </main>
+            <RouterLink to="/counter" >
+              <button class="startButton">click to start</button>
+            </RouterLink>
+         
+  </section>
 </template>
 
-<style scoped>
-h1{
-}
-@media (max-width: 600px){
-    h1{
-        font-size: 2rem;
-    }
-}
-</style>
+
--- src/views/counter.vue
+++ src/views/counter.vue
@@ -14,46 +14,33 @@ const { increment, decrement, reset, setValue } = useCounter(num, store)
 </script>
 
 <template>
-    <div class="h-screen pb-20 px-3 ">
-        <h1 class="text-white w-8/12 mx-auto mt-8 text-center font-bold text-4xl">Counter Page</h1>
+    <div>
+        <h1>Counter</h1>
 
-        <div class="flex justify-evenly items-center flex-col mt-10 gap-5 h-5/6  text-white">
-            <div class="h-40 rounded-md centered mb-3 ">
-                <h1 class="text-6xl font-extrabold px-2 text-white"> {{ count }}</h1>
-            </div>
+        
+                <h1 > {{ count }}</h1>
+           
 
-            <div class="flex flex-col mb-4  w-7/12  gap=5">
-                <div class=" centered w-8/12 mx-auto gap-3">
+           
                     <input type="text" class="h-10 text-black px-2  outline-none in rounded-md  text-2xl" placeholder=" value" v-model="num" />
                     <button class=" text-2xl to px-3 p-auto rounded-md " @click="setValue(num)">Add</button>
-                </div>
+              
 
 
 
-                <div>
                     <div class="centered mt-6 gap-5">
-                        <button @click="decrement" class="  px-2 py-3 text-x bg-green-500 font-bold rounded-md">Decrement</button>
-                        <button @click="reset" class=" px-2 py-3 text-x bg-red-500 font-bold rounded-md">Reset</button>
-                        <button @click="increment" class=" px-2 py-3 text-x bg-green-500 font-bold rounded-md">Increment</button>
+                        <button @click="decrement">Decrement</button>
+                        <button @click="reset" >Reset</button>
+                        <button @click="increment" >Increment</button>
                     </div>
                 </div>
-            </div>
-        </div>
+         
 
-        <div class="px-3 py-6 w-10/12 md:w-4/12 mx-auto homing text-center rounded-md h-10 centered  text-white font-bold mb-20">
-            <router-link to='/' class="">
+      
+            <router-link to='/'>
                 Return Home
             </router-link>
-        </div>
-    </div>
+       
+    
 </template>
 
-<style scoped>
-.in{
-    width: 120px;
-    height: 60px;
-}
-.to{
-    height: 62px;
-}
-</style>
